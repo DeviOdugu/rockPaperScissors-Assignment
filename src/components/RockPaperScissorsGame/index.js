7feacb39a496508bcb54dd133c40eb600536fbd3
Devi Odugu <deviodugu544@gmail.com>
2024-02-20 12:25:21 +0000

    solution

---
+++ src/components/RockPaperScissorsGame/index.js
@@ -0,0 +1,197 @@
+import {Component} from 'react'
+import Popup from 'reactjs-popup'
+import {
+  Container,
+  Card,
+  Container1,
+  RockImage,
+  ScissorImage,
+  PaperImage,
+  PopupContainer,
+  RulesImage,
+  CloseIcon,
+  RulesButton,
+  Container2,
+  CardContainer,
+  ScoreCard,
+  DivContainer,
+  Score,
+  Image1,
+  Image2,
+  ResultContainer,
+  ImageContainer,
+  Text1,
+  Text2,
+  ResultText,
+  ResultButton,
+  Button,
+} from './styledComponents'
+
+class RockPaperScissorsGame extends Component {
+  state = {
+    count: 0,
+    resultText: '',
+    yourChoice: '',
+    opponentChoice: '',
+    displayResult: false,
+  }
+
+  onClickImage = imageId => {
+    // console.log(imageId)
+
+    const {choicesList} = this.props
+    const {count} = this.state
+
+    const randomNumber = Math.floor(Math.random() * choicesList.length)
+    const opponentChoiceImage = choicesList[randomNumber].imageUrl
+    const opponentId = choicesList[randomNumber].id
+    // console.log(opponentId)
+    // console.log(opponentChoiceImage)
+
+    const yourChoiceImage = choicesList.filter(
+      eachChoice => eachChoice.id === imageId,
+    )[0].imageUrl
+    // console.log(yourChoiceImage)
+
+    if (imageId === opponentId) {
+      this.setState({
+        resultText: 'IT IS DRAW',
+      })
+    } else if (
+      (imageId === 'PAPER' && opponentId === 'ROCK') ||
+      (imageId === 'SCISSORS' && opponentId === 'PAPER') ||
+      (imageId === 'ROCK' && opponentId === 'SCISSORS')
+    ) {
+      this.setState({
+        resultText: 'YOU WON',
+        count: count + 1,
+      })
+    } else if (
+      (imageId === 'SCISSORS' && opponentId === 'ROCK') ||
+      (imageId === 'ROCK' && opponentId === 'PAPER') ||
+      (imageId === 'PAPER' && opponentId === 'SCISSORS')
+    ) {
+      this.setState({resultText: 'YOU LOSE', count: count - 1})
+    }
+    this.setState({
+      yourChoice: yourChoiceImage,
+      opponentChoice: opponentChoiceImage,
+      displayResult: true,
+    })
+  }
+
+  onClickPlayAgainButton = () => {
+    this.setState({displayResult: false})
+  }
+
+  render() {
+    const {choicesList} = this.props
+    const {
+      count,
+      resultText,
+      yourChoice,
+      opponentChoice,
+      displayResult,
+    } = this.state
+    return (
+      <Container>
+        <Card>
+          <CardContainer>
+            <DivContainer>
+              <h1>
+                ROCK
+                <br />
+                PAPER
+                <br />
+                SCISSORS
+              </h1>
+            </DivContainer>
+            <ScoreCard>
+              <p>Score</p>
+              <Score>{count}</Score>
+            </ScoreCard>
+          </CardContainer>
+        </Card>
+        {!displayResult && (
+          <>
+            <Container1>
+              <Button
+                tytpe="button"
+                data-testid="rockButton"
+                onClick={() => this.onClickImage(choicesList[0].id)}
+              >
+                <RockImage
+                  src={choicesList[0].imageUrl}
+                  alt={choicesList[0].id}
+                />
+              </Button>
+              <Button
+                type="button"
+                data-testid="scissorsButton"
+                onClick={() => this.onClickImage(choicesList[1].id)}
+              >
+                <ScissorImage
+                  src={choicesList[1].imageUrl}
+                  alt={choicesList[1].id}
+                  onClick={() => this.onClickImage(choicesList[1].id)}
+                />
+              </Button>
+            </Container1>
+            <Container1>
+              <Button
+                type="button"
+                data-testid="paperButton"
+                onClick={() => this.onClickImage(choicesList[2].id)}
+              >
+                <PaperImage
+                  src={choicesList[2].imageUrl}
+                  alt={choicesList[2].id}
+                />
+              </Button>
+            </Container1>
+          </>
+        )}
+        {displayResult && (
+          <ResultContainer>
+            <ImageContainer>
+              <div>
+                <Text1>YOU</Text1>
+                <Image1 src={yourChoice} alt="your choice" />
+              </div>
+              <div>
+                <Text2>OPPONENT</Text2>
+                <Image2 src={opponentChoice} alt="opponent choice" />
+              </div>
+            </ImageContainer>
+            <ResultText>{resultText}</ResultText>
+            <ResultButton type="button" onClick={this.onClickPlayAgainButton}>
+              PLAY AGAIN
+            </ResultButton>
+          </ResultContainer>
+        )}
+        <Popup
+          modal
+          trigger={
+            <Container2>
+              <RulesButton type="button">RULES</RulesButton>
+            </Container2>
+          }
+        >
+          {close => (
+            <PopupContainer>
+              <RulesImage
+                src="https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rules-image.png"
+                alt="rules"
+                height="250px"
+                width="300px"
+              />
+              <CloseIcon onClick={() => close()} />
+            </PopupContainer>
+          )}
+        </Popup>
+      </Container>
+    )
+  }
+}
+
+export default RockPaperScissorsGame
---
+++ src/components/RockPaperScissorsGame/styledComponents.js
@@ -0,0 +1,127 @@
+import styled from 'styled-components'
+import {RiCloseLine} from 'react-icons/ri'
+
+export const Container = styled.div`
+  background-color: #223a5f;
+  padding: 20px;
+`
+export const Card = styled.div`
+  border: solid #ffffff 2px;
+  padding: 5px;
+  border-radius: 6px;
+`
+export const CardContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const DivContainer = styled.div`
+  color: #ffffff;
+  font-family: 'Bree Serif';
+  font-size: 10px;
+  padding-left: 10px;
+  font-size: 7px;
+`
+export const ScoreCard = styled.div`
+  background-color: #ffffff;
+  border-radius: 6px;
+  color: #223a5f;
+  font-family: 'Roboto';
+  text-align: center;
+  font-size: 10px;
+  font-weight: 700;
+  margin: 2px 10px 2px 0px;
+  padding: 0px 30px 0px 30px;
+`
+export const Score = styled.p`
+  font-size: 18px;
+  font-family: 'Roboto';
+`
+export const Container1 = styled.div`
+  display: flex;
+  justify-content: center;
+`
+export const Button = styled.button`
+  border: none;
+  background-color: transparent;
+`
+export const RockImage = styled.img`
+  height: 100px;
+  width: 100px;
+  margin-right: 30px;
+  margin-top: 100px;
+`
+export const ScissorImage = styled.img`
+  height: 100px;
+  width: 100px;
+  margin-top: 100px;
+`
+export const PaperImage = styled.img`
+  height: 100px;
+  width: 100px;
+  margin-top: 30px;
+  margin-bottom: 120px;
+`
+export const Container2 = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+export const RulesButton = styled.button`
+  color: #223a5f;
+  font-family: 'Bree Serif';
+  background-color: #ffffff;
+  font-size: 10px;
+  border-radius: 4px;
+  padding: 5px 10px 5px 10px;
+`
+export const PopupContainer = styled.div`
+  background-color: #ffffff;
+  padding: 5px;
+  display: flex;
+  padding-left: 30px;
+`
+export const RulesImage = styled.img`
+  margin: 10px 18px 7px 5px;
+`
+export const CloseIcon = styled(RiCloseLine)`
+  height: 10px;
+  width: 10px;
+`
+export const ResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  font-family: 'Roboto';
+  font-weight: 700;
+  margin: 120px 0px 120px 0px;
+`
+export const ImageContainer = styled.div`
+  display: flex;
+`
+export const Text1 = styled.p`
+  padding-left: 30px;
+`
+export const Image1 = styled.img`
+  height: 100px;
+  width: 100px;
+  margin-right: 100px;
+`
+export const Text2 = styled.p`
+  padding-left: 13px;
+`
+export const Image2 = styled.img`
+  height: 100px;
+  width: 100px;
+`
+export const ResultText = styled.p`
+  font-size: 20px;
+`
+export const ResultButton = styled.button`
+  font-size: 7px;
+  color: #223a5f;
+  background-color: #ffffff;
+  padding: 5px 20px 5px 20px;
+  border-radius: 6px;
+  font-family: 'Bree Serif';
+`
